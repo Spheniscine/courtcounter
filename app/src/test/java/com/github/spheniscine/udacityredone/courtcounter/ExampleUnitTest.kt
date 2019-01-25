e27c3e692d65c78a999b1357146b7b9ff5dc0a52
spheniscine <spheniscine@gmail.com>
2019-01-25 13:56:02 +0800

    improve delegate type checking

--- app/src/test/java/com/github/spheniscine/udacityredone/courtcounter/ExampleUnitTest.kt
+++ app/src/test/java/com/github/spheniscine/udacityredone/courtcounter/ExampleUnitTest.kt
@@ -1,8 +1,13 @@
 package com.github.spheniscine.udacityredone.courtcounter
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import com.github.spheniscine.udacityredone.courtcounter.util.delegate
+import org.junit.Assert
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +15,24 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun `LiveData delegate test`() {
+        val ld = MutableLiveData<Boolean>()
+        var property: Boolean by ld.delegate()
+
+        assertTrue(runCatching { property }.isFailure)
+
+        property = true
+
+        assertTrue(property)
     }
 }
