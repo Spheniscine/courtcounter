0d23ef8ef1497e3127df9d0947f87fed9d6b5766
spheniscine <spheniscine@gmail.com>
2019-01-25 10:54:53 +0800

    Forgot to stage changes

---
+++ app/src/main/java/com/github/spheniscine/udacityredone/courtcounter/AllOpen.kt
@@ -0,0 +1,5 @@
+package com.github.spheniscine.udacityredone.courtcounter
+
+@Retention(AnnotationRetention.SOURCE)
+@Target(AnnotationTarget.CLASS)
+annotation class AllOpen
--- app/src/main/java/com/github/spheniscine/udacityredone/courtcounter/ui/MainViewModel.kt
+++ app/src/main/java/com/github/spheniscine/udacityredone/courtcounter/ui/MainViewModel.kt
@@ -1,25 +1,27 @@
 package com.github.spheniscine.udacityredone.courtcounter.ui
 
 import androidx.lifecycle.ViewModel
+import com.github.spheniscine.udacityredone.courtcounter.AllOpen
 import com.github.spheniscine.udacityredone.courtcounter.util.MutableLiveData
 import com.github.spheniscine.udacityredone.courtcounter.util.delegate
 
-open class MainViewModel : ViewModel() {
-    open val scoreTeamA_ld = MutableLiveData(0)
-    open var scoreTeamA by scoreTeamA_ld.delegate()
+@AllOpen
+class MainViewModel : ViewModel() {
+    val scoreTeamA_ld = MutableLiveData(0)
+    var scoreTeamA by scoreTeamA_ld.delegate()
 
-    open val scoreTeamB_ld = MutableLiveData(0)
-    open var scoreTeamB by scoreTeamB_ld.delegate()
+    val scoreTeamB_ld = MutableLiveData(0)
+    var scoreTeamB by scoreTeamB_ld.delegate()
 
-    open fun addOneForTeamA() { scoreTeamA += 1 }
-    open fun addTwoForTeamA() { scoreTeamA += 2 }
-    open fun addThreeForTeamA() { scoreTeamA += 3 }
+    fun addOneForTeamA() { scoreTeamA += 1 }
+    fun addTwoForTeamA() { scoreTeamA += 2 }
+    fun addThreeForTeamA() { scoreTeamA += 3 }
 
-    open fun addOneForTeamB() { scoreTeamB += 1 }
-    open fun addTwoForTeamB() { scoreTeamB += 2 }
-    open fun addThreeForTeamB() { scoreTeamB += 3 }
+    fun addOneForTeamB() { scoreTeamB += 1 }
+    fun addTwoForTeamB() { scoreTeamB += 2 }
+    fun addThreeForTeamB() { scoreTeamB += 3 }
 
-    open fun resetScore() {
+    fun resetScore() {
         scoreTeamA = 0
         scoreTeamB = 0
     }
